b6b5a2e027ea9c243153b85ba5e7f3e715b547e8
Scott Christensen <scottmc526@gmail.com>
2016-01-05 16:36:08 -0700

    song that never ends finished

---
+++ 02-song-that-doesnt-end/song-that-never-ends/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'This is the song that never ends' });
+});
+
+router.get('/verse2', function (req, res, next) {
+  res.render('verse2', {title: 'It goes on and on my friends'})
+})
+
+router.get('/verse3', function(req, res, next){
+  res.render('verse3', {title: 'Some people, started singing it not knowing what it was'})
+})
+
+router.get('/verse4', function(req, res, next){
+  res.render('verse4', {title:'And they will continue singing it forever just because'})
+})
+
+module.exports = router;
