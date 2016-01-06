2fc052457268af80d2e8d69d75d0fa2d696159ff
Scott Christensen <scottmc526@gmail.com>
2016-01-06 10:47:31 -0700

    added lib, src, and show files

--- 03-hipstafy/hipstafy/routes/index.js
+++ 03-hipstafy/hipstafy/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var hipstafy = require('../src/hipstafy');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//var results = hipstafyFunction(req.body.userinput)
+//res.render('show', {results: results})
 
 module.exports = router;
---
+++ 03-hipstafy/hipstafy/src/hipstafy.js
@@ -0,0 +1,8 @@
+var hipster = require('../lib/snippets');
+
+
+function hipsterIt (userInput) {
+  console.log(userInput.split(' '));
+}
+
+hipsterIt('Hello There people')
